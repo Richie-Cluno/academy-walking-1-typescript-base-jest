33dc1d39fda84fc30a9ed588cfad99125112a9b4
Richie <prateek.choudhury@cluno.com>
2021-06-08 11:40:32 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/fibonacci.ts
+++ src/main/fibonacci.ts
@@ -1,19 +1,5 @@
 export function FibonacciAtPos(position: number): number{
-    if (position === 0){
-        return 0;
-    }
-    if(position === 1) {
-        return 1;
-    }
-    let i = 2;
-    let a = 0;
-    let b = 1
-    let result = 0
-    while(i <= position) {
-        result = a + b;
-        a = b;
-        b = result;
-        i = i + 1;
-    }
-    return result;
+    if (position < 2) return position;
+
+    return FibonacciAtPos(position - 1) + FibonacciAtPos(position - 2);
 }
